312de7448c8285704692dd9958392b907aa268f7
henry <aengrhenry@gmail.com>
2022-04-14 16:24:31 +0100

    stripe payment frontend implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Product from './components/Product';
+import {
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+import Message from './components/Message';
+import Checkout from './components/Checkout';
+
 
 function App() {
-  return (
+
+  // const [message, setMessage]=useState("")
+  // // const location=useLocation();
+ 
+  // useEffect(() => {
+  //   // Check to see if this is a redirect back from Checkout
+  //   const query = new URLSearchParams(window.location.search);
+  
+  //     if (query.get("success")) {
+  //       setMessage("Order placed! You will receive an email confirmation.");
+  //     }
+  
+  //     if (query.get("canceled")) {
+  //       setMessage(
+  //         "Order canceled -- continue to shop around and checkout when you're ready."
+  //       );
+  //       }
+   
+  // }, []);
+
+ 
+  return  (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* {  message ? (
+            <Message message={message}/>
+      ):( */}
+    <Routes> 
+          <Route exact path='/success' element={<Message/>}/>      
+          <Route  path="/:product_id" element={<Product/>}/> 
+          <Route exact path="checkout/:prod_id" element={<Checkout/>}/>         
+    </Routes>
+      {/* )} */}
     </div>
+    
   );
 }
 
